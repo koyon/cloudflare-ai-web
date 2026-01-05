6c44883f49cddfb89bc5c0756997d1ca57d8da99
koyon <cya8989@gmail.com>
2026-01-05 11:39:51 +0900

    edit: add edge to proxy.tx

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,6 @@
 import { type NextRequest, NextResponse } from "next/server";
 
+export const runtime = 'edge';
 export function proxy(request: NextRequest) {
   if (!process.env.APP_PASSWORD) {
     return NextResponse.next();
