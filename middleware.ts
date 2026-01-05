323a92b6689206d8fcf0b217f650fdd20c6f2ac1
koyon <cya8989@gmail.com>
2026-01-05 12:01:09 +0900

    edit: combine proxy to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,17 @@
-import { proxy } from './proxy'; 
+import { type NextRequest, NextResponse } from "next/server";
 
 export const runtime = 'edge';
 
-export { config } from './proxy';
+export function middleware(request: NextRequest) {
+  if (!process.env.APP_PASSWORD) {
+    return NextResponse.next();
+  }
+  const password = request.headers.get("Authorization");
+  if (password !== process.env.APP_PASSWORD) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+}
 
-export default proxy;
+export const config = {
+  matcher: "/api/:path*",
+};
