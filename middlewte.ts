ca817d3ed603fd2a5d8b6adaa1db50a1b13bd997
koyon <cya8989@gmail.com>
2026-01-05 11:50:39 +0900

    edit: middleware.ts change

--- middlewte.ts
+++ middlewte.ts
@@ -3,3 +3,5 @@ import { proxy } from './proxy';
 export const runtime = 'edge';
 
 export { config } from './proxy';
+
+export default proxy;
--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,5 @@
 import { type NextRequest, NextResponse } from "next/server";
 
-export const runtime = 'edge';
 export function proxy(request: NextRequest) {
   if (!process.env.APP_PASSWORD) {
     return NextResponse.next();
